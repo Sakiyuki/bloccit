b94cac44d3e2cf6c3300bef7566b6161989830bd
Bryant Caldwell <cwellionaire@gmail.com>
2016-05-31 17:46:17 -0400

    checkpoint-34-topics-posts completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,4 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-  end
 
   def show
     @post = Post.find(params[:id])
@@ -9,6 +6,7 @@ class PostsController < ApplicationController
 
   def new
     # #7
+    @topic = Topic.find(params[:topic_id])
     @post = Post.new
   end
 
@@ -16,11 +14,13 @@ class PostsController < ApplicationController
     @post = Post.new
     @post.title = params[:post][:title]
     @post.body = params[:post][:body]
+    @topic = Topic.find(params[:topic_id])
+    @post.topic = @topic
     # #10
     if @post.save
     # #11
       flash[:notice] = "Post was saved successfully."
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
   # #12
       flash.now[:alert] = "There was an error saving the post. Please try again."
@@ -39,7 +39,7 @@ class PostsController < ApplicationController
 
     if @post.save
       flash[:notice] = "Post was updated successfully."
-      redirect_to @post
+      redirect_to [@post.topic, @post]
     else
       flash.now[:alert] = "Ther was an error saving the post. Pleas try again."
       render :edit
@@ -52,7 +52,7 @@ class PostsController < ApplicationController
 # #8
     if @post.destroy
       flash[:notice] = "\"#{@post.title}\" was deleted successfully."
-      redirect_to posts_path
+      redirect_to @post.topic
     else
       flash.now[:alert] = "There was an error deleting the post."
       render :show
