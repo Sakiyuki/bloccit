4e10400c60c89274802cd992c88b3202fa36597b
Bryant Caldwell <cwellionaire@gmail.com>
2016-06-26 13:18:43 -0400

    started on checkpoint-44-favorites

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   # def posts
     # Post.find(user: self.id)
