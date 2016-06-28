39fdc4df535ae1e8e9281b425611215e6f7bc70a
Bryant Caldwell <cwellionaire@gmail.com>
2016-06-28 11:15:46 -0400

    completed checkpoint-44-favorites

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   resources :posts, only: [] do
 # #5
     resources :comments, only: [:create, :destroy]
+    resources :favorites, only: [:create, :destroy]
 
     post '/up_vote' => 'votes#up_vote', as: :up_vote
     post '/down_vote' => 'votes#down_vote', as: :down_vote
