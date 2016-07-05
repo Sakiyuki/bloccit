12333e699ddd43b85f563aecfd963dbb2275db3a
Bryant Caldwell <cwellionaire@gmail.com>
2016-07-05 10:33:11 -0400

    completed checkpoint-46-private_topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -17,6 +17,8 @@ class Topic < ActiveRecord::Base
 # #13
   has_many :labels, through: :labelings
 
+  scope :visible_to, -> (user) { user ? all : where(public: true) }
+
   validates :name, length: { minimum: 5 }, presence: true
   validates :description, length: { minimum: 15 }, presence: true
 end
