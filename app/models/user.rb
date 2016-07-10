4829db58e77e6466a9f434ab932c2e3947183a79
Bryant Caldwell <cwellionaire@gmail.com>
2016-07-10 13:59:33 -0400

    completed checkpoint-48-API-retreive

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,12 +19,12 @@ class User < ActiveRecord::Base
 
   # def posts
     # Post.find(user: self.id)
-  # end
-
+  before_create :generate_auth_token
   # #2
   before_save { self.email = email.downcase if email.present? }
   before_save { self.role ||= :member }
   # before_save { self.role ||= :moderator }
+
 
   # #3
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
@@ -49,5 +49,12 @@ class User < ActiveRecord::Base
   def avatar_url(size)
     gravatar_id = Digest::MD5::hexdigest(self.email).downcase
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+  end
+
+  def generate_auth_token
+    loop do
+      self.auth_token = SecureRandom.base64(64)
+      break unless User.find_by(auth_token: auth_token)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,14 @@ Rails.application.routes.draw do
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
+
+  # #19
+    namespace :api do
+      namespace :v1 do
+        resources :users, only: [:index, :show]
+        resources :topics, only: [:index, :show]
+      end
+    end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
